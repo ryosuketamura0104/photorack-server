781d0bf4224f68009804071c427c953ca597e50c
ryosuketamura0104 <ryosuke.tamura0104@gmail.com>
2023-11-21 01:03:58 +0900

    ルートを修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('tests', TestController::class);
 
 Route::get('/', function () {
     return view('welcome');
