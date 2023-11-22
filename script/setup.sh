b2a2318b6c570941843b309cef9541b2639e1271
ryosuketamura0104 <r.tamura@octawell.co.jp>
2023-11-22 12:35:54 +0900

    update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -6,6 +6,10 @@ docker ps
 
 docker compose exec app chown www-data storage/ -R
 
+docker compose exec app composer update --no-scripts
+
+docker compose exec app composer dump-autoload
+
 open http://localhost:8000
 
 echo "Done"
