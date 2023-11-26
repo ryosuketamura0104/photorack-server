eb4afefef029126afcbb5d3b0f8b2afe1a592084
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2023-11-26 16:35:24 +0900

    ログイン機能一部すすめた

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,11 @@ Route::resource('tests', TestController::class);
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*  https://qiita.com/mumucochimu/items/3d4ff8fa8fcb9826339f
+    https://qiita.com/nakaji0210/items/036877bf6fe71f5cf9ef */
+// Auth::routes();
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
