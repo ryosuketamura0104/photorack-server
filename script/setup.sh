fa43ef7412292dfd37728fddd685ebf6668ecc39
nzmzrw <91615947+nzmzrw@users.noreply.github.com>
2023-11-30 00:03:48 +0900

    update setup npm install

--- script/setup.sh
+++ script/setup.sh
@@ -10,6 +10,10 @@ docker compose exec app composer update --no-scripts
 
 docker compose exec app composer dump-autoload
 
+npm install
+
+npm run watch-poll
+
 open http://localhost:8000
 
 echo "Done"
